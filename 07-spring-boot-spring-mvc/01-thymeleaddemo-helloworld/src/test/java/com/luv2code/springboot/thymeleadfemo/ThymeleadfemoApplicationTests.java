ee24f0fa64c98abad8e578460e4190eb9fb5a34b
cu <harrierraum@hotmail.com>
2023-12-12 16:42:16 +0100

    create mvc form

---
+++ 07-spring-boot-spring-mvc/01-thymeleaddemo-helloworld/src/test/java/com/luv2code/springboot/thymeleadfemo/ThymeleadfemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.thymeleadfemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleadfemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
